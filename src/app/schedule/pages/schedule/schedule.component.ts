f59c83fe44a0facb1968c8ce8763df9bd18645da
louisyoong <louis@greenroom.com.my>
2019-05-28 11:56:50 +0800

    Add schedule details

--- src/app/schedule/pages/schedule/schedule.component.ts
+++ src/app/schedule/pages/schedule/schedule.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {Observable} from 'rxjs';
 
 @Component({
-  selector: 'app-schedule',
-  templateUrl: './schedule.component.html',
-  styleUrls: ['./schedule.component.scss']
+    selector    : 'app-schedule',
+    templateUrl : './schedule.component.html',
+    styleUrls   : [ './schedule.component.scss' ]
 })
 export class ScheduleComponent implements OnInit {
     items: Observable<any[]>;
 
     constructor(db: AngularFirestore) {
-        this.items = db.collection('schedule').valueChanges();
+        this.items = db.collection('schedule', ref => ref.orderBy('hrs', 'asc')).valueChanges();
     }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
