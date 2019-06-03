b4e405a1b26f54e40712c5d481172f7f220f259a
louisyoong <louis@greenroom.com.my>
2019-06-03 11:37:33 +0800

    Change route name

--- src/app/attending/attending.module.ts
+++ src/app/attending/attending.module.ts
@@ -5,7 +5,7 @@ import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
     {
-        path     : 'whatToExpect',
+        path     : 'what-to-expect',
         children : [
             {
                 path      : '',
